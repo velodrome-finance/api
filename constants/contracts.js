96adc2916a49b2d4f75736e0197f26e52dc7d469
Anton Nell <antonnell89@gmail.com>
2022-01-31 11:27:18 +0200

    Updated to latest contracts.
    Signed-off-by: Anton Nell <antonnell89@gmail.com>

--- constants/contracts.js
+++ constants/contracts.js
@@ -1,19 +1,13 @@
 const abis = require("./abis")
 
-module.exports.FACTORY_ADDRESS = '0xd231865CE0eDB0079F0D0d7BB2E647458aa60d11'
+module.exports.FACTORY_ADDRESS = '0xff81f7b1F18E7B0Af0cE970f3dA7763e7bED3fe5'
 module.exports.FACTORY_ABI = abis.factoryABI
 
-module.exports.ROUTER_ADDRESS = '0x612003865305a7E9915Cb3b1b06cC2485F03dD29'
+module.exports.ROUTER_ADDRESS = '0xC9f92bF3add3d462b9f0d937Fff4878e076beE09'
 module.exports.ROUTER_ABI = abis.routerABI
 
-module.exports.VE_DIST_ADDRESS = '0x4Dc46A325B141751E09871279F9c7Fe214DA422C'
-module.exports.VE_DIST_ABI = abis.veDistABI
-
-module.exports.GAUGES_ADDRESS = '0xd2F7fF8e5b362bafE2b57a82c5865B4355F884Ae'
+module.exports.GAUGES_ADDRESS = '0x571f572D6046A9CEF7B7f629D79cF8cFf6E164E5'
 module.exports.GAUGES_ABI = abis.gaugesABI
-
-module.exports.MINTER_ADDRESS = '0x3230F944a26288f49F5010b11BA96b0b9dC84e79'
-module.exports.MINTER_ABI = abis.minterABI
 
 module.exports.ERC20_ABI = abis.erc20ABI
 module.exports.PAIR_ABI = abis.pairABI
