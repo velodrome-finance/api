a6359d586726961e57b6f4ce3f125c5a47cbc204
Anton Nell <antonnell89@gmail.com>
2022-02-09 09:01:29 +0200

    Latest Contracts
    Signed-off-by: Anton Nell <antonnell89@gmail.com>

--- constants/contractsTestnet.js
+++ constants/contractsTestnet.js
@@ -1,12 +1,12 @@
 const abis = require("./abis")
 
-module.exports.FACTORY_ADDRESS = '0xe6367958e8297cb8bdC4B99BC15Ae402Ebf41baA'
+module.exports.FACTORY_ADDRESS = '0x7dbA586e00D868EeFA13fC549B63D62b4a45a244'
 module.exports.FACTORY_ABI = abis.factoryABI
 
-module.exports.ROUTER_ADDRESS = '0x04583f12bF185f11d1B3783894Acaa29dCE6547A'
+module.exports.ROUTER_ADDRESS = '0xD1D173c5d1de3175B32d3dDCB265BD00Ea9fEDf5'
 module.exports.ROUTER_ABI = abis.routerABI
 
-module.exports.GAUGES_ADDRESS = '0x3c137d25FAC210C211Eb523da31542B8A115f7F8'
+module.exports.GAUGES_ADDRESS = '0xDA0eBF84F7A52A06EaeB0409F139D6442262F056'
 module.exports.GAUGES_ABI = abis.gaugesABI
 
 module.exports.ERC20_ABI = abis.erc20ABI
