c1671824dc5eb27a9a14c692b3db65dbe9394e96
Stas SUȘCOV <stas@nerd.ro>
2022-05-22 20:21:26 +0100

    Updated setup and example envs.

--- app/setup.py
+++ app/setup.py
@@ -11,13 +11,16 @@ setup(
         'falcon == 3.1',
         'bjoern == 3.2.1',
         'web3',
-        'multicall == 0.4',
+        'multicall == 0.5.1',
         'redis == 4.2.2',
         'fakeredis == 1.7.4'
     ),
+    # dependency_links = [
+    #     'https://codeload.github.com/velodrome-finance/multicall.py/zip/refs/heads/add_optimism_kovan#egg=multicall-0.5.1'
+    # ],
     tests_require=(
         'flake8',
         'coverage'
     ),
-    version = ":versiontools:.settings"
+    version=":versiontools:.settings"
 )
