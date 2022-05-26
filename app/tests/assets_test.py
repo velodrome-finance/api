b967ecc2e178c3042e2a515ce587d4ae9f0c9c45
Stas SUȘCOV <stas@nerd.ro>
2022-05-26 17:50:52 +0100

    Ignore assets from other chains.

--- app/tests/assets_test.py
+++ app/tests/assets_test.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 
+from web3.auto import w3
+
 from .helpers import AppTestCase
 
 
 class AssetsTestCase(AppTestCase):
     def test_get(self):
         result = self.simulate_get('/api/v1/assets')
+        our_chain_id = w3.eth.chain_id
 
         self.assertEqual(type(result.json['data']), list)
         self.assertTrue(len(result.json['data']) > 1)
+
+        for token in result.json['data']:
+            self.assertEqual(token['chainId'], our_chain_id)
