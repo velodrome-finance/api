9c29dfd4493e04b208031098360ff500acdb004a
Stas SUȘCOV <stas@nerd.ro>
2022-05-29 22:19:31 +0100

    More tests and fixes.

--- app/tests/pairs_test.py
+++ app/tests/pairs_test.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 
+from app.pairs import Pair
 from .helpers import AppTestCase
 
 
 class PairsTestCase(AppTestCase):
     def test_get(self):
         result = self.simulate_get('/api/v1/pairs')
+
+        self.assertEqual(type(result.json['data']), list)
+        self.assertTrue(len(result.json['data']) > 1)
+
+    def test_get_with_pair_address(self):
+        pair = next(Pair.all())
+        result = self.simulate_get(
+            '/api/v1/pairs?pair_address={}'.format(pair.address)
+        )
 
         self.assertEqual(type(result.json['data']), list)
         self.assertTrue(len(result.json['data']) > 1)
