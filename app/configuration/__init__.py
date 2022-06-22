7a66a0fc9d1a534a70db03d718f0d932033fcf34
Mukesh <mukesh@missmalini.com>
2022-06-22 09:20:55 +0530

    add ability to add multiple route assets

--- app/configuration/__init__.py
+++ app/configuration/__init__.py
@@ -7,7 +7,7 @@ from versiontools import Version
 
 from app import __version__
 from app.pairs import Pair, Token
-from app.settings import CACHE, DEFAULT_TOKEN_ADDRESS, STABLE_TOKEN_ADDRESS
+from app.settings import CACHE, DEFAULT_TOKEN_ADDRESS, STABLE_TOKEN_ADDRESS, ROUTE_TOKENS
 
 
 class Configuration(object):
@@ -16,13 +16,15 @@ class Configuration(object):
     def on_get(self, req, resp):
         default_token = Token.find(DEFAULT_TOKEN_ADDRESS)
         stable_token = Token.find(STABLE_TOKEN_ADDRESS)
+        route_tokens = [Token.find(token.lower()) for token in ROUTE_TOKENS]
+        route_token_data = [token._data for token in route_tokens]
         tvl = sum(map(lambda p: p.tvl, Pair.all()))
         max_apr = max(map(lambda p: p.apr, Pair.all()))
 
         resp.status = falcon.HTTP_200
         resp.text = json.dumps(
             dict(
-                data=[default_token._data, stable_token._data],
+                data=[default_token._data, stable_token._data, *route_token_data],
                 meta=dict(
                     tvl=tvl,
                     max_apr=max_apr,
