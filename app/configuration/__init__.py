adcdb1d93f062cc70835024da3f326583545f31f
Stas SUȘCOV <stas@nerd.ro>
2022-06-22 13:16:57 +0300

    Linter cleanup, naming updates.

--- app/configuration/__init__.py
+++ app/configuration/__init__.py
@@ -7,7 +7,8 @@ from versiontools import Version
 
 from app import __version__
 from app.pairs import Pair, Token
-from app.settings import CACHE, DEFAULT_TOKEN_ADDRESS, STABLE_TOKEN_ADDRESS, ROUTE_TOKENS
+from app.settings import CACHE, DEFAULT_TOKEN_ADDRESS, STABLE_TOKEN_ADDRESS,\
+    ROUTE_TOKEN_ADDRESSES
 
 
 class Configuration(object):
@@ -16,7 +17,7 @@ class Configuration(object):
     def on_get(self, req, resp):
         default_token = Token.find(DEFAULT_TOKEN_ADDRESS)
         stable_token = Token.find(STABLE_TOKEN_ADDRESS)
-        route_tokens = [Token.find(token.lower()) for token in ROUTE_TOKENS]
+        route_tokens = [Token.find(token) for token in ROUTE_TOKEN_ADDRESSES]
         route_token_data = [token._data for token in route_tokens]
         tvl = sum(map(lambda p: p.tvl, Pair.all()))
         max_apr = max(map(lambda p: p.apr, Pair.all()))
@@ -24,7 +25,11 @@ class Configuration(object):
         resp.status = falcon.HTTP_200
         resp.text = json.dumps(
             dict(
-                data=[default_token._data, stable_token._data, *route_token_data],
+                data=[
+                    default_token._data,
+                    stable_token._data,
+                    *route_token_data
+                ],
                 meta=dict(
                     tvl=tvl,
                     max_apr=max_apr,
