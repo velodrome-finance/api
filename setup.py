1db2126717532beaadc1999700242eb8048012ac
Stas SUȘCOV <stas@nerd.ro>
2022-07-23 15:23:34 +0100

    Added Honeybadger for exception notifications.
    Closes #31

--- setup.py
+++ setup.py
@@ -16,7 +16,8 @@ setup(
         'multicall @ https://codeload.github.com/velodrome-finance/multicall.py/zip/refs/heads/optimism_version_bump#egg=multicall-0.5.2',  # noqa
         'redis == 4.2.2',
         'fakeredis == 1.7.4',
-        'walrus == 0.9.1'
+        'walrus == 0.9.1',
+        'honeybadger == 0.8.0'
     ),
     version=":versiontools:app"
 )
