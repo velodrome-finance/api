806efb650012cd1244486f31104619fcc778ebc0
Stas SUȘCOV <stas@nerd.ro>
2022-07-25 22:01:45 +0100

    Added rewards to the account endpoint.

--- app/venfts/__init__.py
+++ app/venfts/__init__.py
@@ -6,6 +6,8 @@ import falcon
 from web3 import Web3
 
 from .model import VeNFT
+from app.pairs import Gauge, Pair, Token
+from app.rewards import BribeReward, EmissionReward, FeeReward
 from app.misc import JSONEncoder
 from app.settings import CACHE, LOGGER, reset_multicall_pool_executor
 
@@ -16,17 +18,59 @@ class Accounts(object):
     CACHE_KEY = 'account:%s:json'
 
     @classmethod
-    def recache(cls, address):
+    def serialize(cls, address):
+        serialized = []
         venfts = VeNFT.from_chain(address)
 
-        data = list(map(lambda nft: nft._data, venfts))
+        reset_multicall_pool_executor()
 
-        serialized = json.dumps(dict(data=data), cls=JSONEncoder)
+        for venft in venfts:
+            data = venft._data
+            data['rewards'] = []
+
+            rewards = list(
+                BribeReward.query(BribeReward.account_address == address)
+            )
+            rewards.extend(
+                list(FeeReward.query(FeeReward.account_address == address))
+            )
+            rewards.extend(
+                list(
+                    EmissionReward.query(
+                        EmissionReward.account_address == address
+                    )
+                )
+            )
+
+            for reward in rewards:
+                rdata = reward._data
+
+                rdata['source'] = \
+                    reward.__class__.__name__.replace('Reward', '')
+
+                if reward.token_address:
+                    rdata['token'] = Token.find(reward.token_address)._data
+
+                if reward.pair_address:
+                    rdata['pair'] = Pair.find(reward.pair_address)._data
+
+                if reward.gauge_address:
+                    rdata['gauge'] = Gauge.find(reward.gauge_address)._data
+
+                data['rewards'].append(rdata)
+
+            serialized.append(data)
+
+        return serialized
+
+    @classmethod
+    def recache(cls, address):
+        serialized = json.dumps(
+            dict(data=cls.serialize(address)), cls=JSONEncoder
+        )
 
         CACHE.setex(cls.CACHE_KEY % address, cls.KEEPALIVE, serialized)
         LOGGER.debug('Cache updated for %s.', cls.CACHE_KEY % address)
-
-        reset_multicall_pool_executor()
 
         return serialized
 
