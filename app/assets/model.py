92bd625d3b5e2bb69c6fde0457198b5e8a6e5689
ethzoomer <ethzoomer@gmail.com>
2022-12-27 10:28:43 -0600

    multichain pricing support

--- app/assets/model.py
+++ app/assets/model.py
@@ -23,6 +23,8 @@ class Token(Model):
     decimals = IntegerField()
     logoURI = TextField()
     price = FloatField(default=0)
+    nativeChainAddress = TextField(default='')
+    nativeChainId = IntegerField(default=0)
 
     # See: https://docs.1inch.io/docs/aggregation-protocol/api/swagger
     AGGREGATOR_ENDPOINT = 'https://api.1inch.io/v4.0/10/quote'
@@ -32,6 +34,15 @@ class Token(Model):
     DEFILLAMA_ENDPOINT = 'https://coins.llama.fi/prices/current/'
     # See: https://api.dev.dex.guru/docs#tag/Token-Finance
     DEXGURU_ENDPOINT = 'https://api.dev.dex.guru/v1/chain/10/tokens/%/market'
+
+    CHAIN_NAMES = {'1': 'ethereum',
+                        '56': 'bsc',
+                        '43114': 'avax',
+                        '42161': 'arbitrum',
+                        '250': 'fantom',
+                        '10': 'optimism',
+                        '137': 'polygon',
+                        '42220': 'celo'}
 
     def dexguru_price_in_stables(self):
         """Returns the price quoted from DexGuru"""
@@ -49,7 +60,17 @@ class Token(Model):
         if self.address == STABLE_TOKEN_ADDRESS:
             return 1.0
 
-        chain_token = 'optimism:' + self.address.lower()
+        if (
+            self.nativeChainAddress != '' and
+            self.nativeChainId != 0 and
+            self.nativeChainAddress is not None and
+            self.nativeChainId is not None
+        ):
+            chain_name = self.CHAIN_NAMES[str(self.nativeChainId)]
+            chain_token = chain_name + ':' + self.nativeChainAddress.lower()
+        else:
+            chain_token = 'optimism:' + self.address.lower()
+
         res = requests.get(self.DEFILLAMA_ENDPOINT + chain_token).json()
         coins = res.get('coins', {})
 
@@ -85,7 +106,15 @@ class Token(Model):
         if self.address == STABLE_TOKEN_ADDRESS:
             return 1.0
 
-        res = requests.get(self.DEXSCREENER_ENDPOINT + self.address).json()
+        if (
+            self.nativeChainAddress != '' and
+            self.nativeChainAddress is not None
+        ):
+            token_address = self.nativeChainAddress.lower()
+        else:
+            token_address = self.address.lower()
+
+        res = requests.get(self.DEXSCREENER_ENDPOINT + token_address).json()
         pairs = res.get('pairs') or []
 
         if len(pairs) == 0:
@@ -202,6 +231,11 @@ class Token(Model):
 
                     if token_data['address'] in IGNORED_TOKEN_ADDRESSES:
                         continue
+
+                    if 'nativeChainAddress' in token_data:
+                        if token_data['nativeChainAddress'] is not None:
+                            addr = token_data['nativeChainAddress'].lower()
+                            token_data['nativeChainAddress'] = addr
 
                     token = cls.create(**token_data)
                     token._update_price()
