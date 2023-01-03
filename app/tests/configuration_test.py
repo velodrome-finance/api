ac8a3cbfff4b4a441dc82a604551f703c8ff9951
Stas SUȘCOV <stas@nerd.ro>
2023-01-03 18:09:28 +0000

    Cleanup conf endpoint. Remove bloat.

--- app/tests/configuration_test.py
+++ app/tests/configuration_test.py
@@ -7,7 +7,5 @@ class ConfigurationTestCase(AppTestCase):
     def test_get(self):
         result = self.simulate_get('/api/v1/configuration')
 
-        self.assertTrue(result.json['meta']['tvl'] > 0)
-        self.assertTrue(result.json['meta']['max_apr'] > 0)
+        self.assertTrue(len(result.json['data']) > 0)
         self.assertIsNotNone(result.json['meta']['version'])
-        self.assertFalse(result.json['meta']['cache'])
